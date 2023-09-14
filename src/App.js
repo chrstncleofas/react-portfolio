48de927d45bf36bf661863f512401ac80c9d1bd8
Dev_tian <c.cleofas@psp.team>
2023-09-14 21:48:00 +0800

    Disable inspect element

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import "./App.css";
 import Navbar from './Navbar';
 import Profile from './Profile';
 import Skills from './Skills';
@@ -8,6 +9,28 @@ import Services from './Services';
 import Project from './Project';
 
 function App() {
+  useEffect(() => {
+    document.addEventListener("contextmenu", function (e) {
+      e.preventDefault();
+    });
+
+    document.onkeydown = function (e) {
+      if (e.keyCode === 123 || (e.ctrlKey && e.shiftKey && e.keyCode === 73) ||
+          (e.ctrlKey && e.shiftKey && e.keyCode === 67) ||
+          (e.ctrlKey && e.shiftKey && e.keyCode === 74) ||
+          (e.ctrlKey && e.keyCode === 85)) {
+        e.preventDefault();
+      }
+    };
+
+    return () => {
+      document.removeEventListener("contextmenu", function (e) {
+        e.preventDefault();
+      });
+      document.onkeydown = null;
+    }
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
